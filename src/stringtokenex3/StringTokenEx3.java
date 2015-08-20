09f200a45cce81bac0e717773109b7cdcffa5c26
CAMT <CAMT@Parinya-PC>
2015-08-20 15:37:51 +0700

    Latest Test

--- src/stringtokenex3/StringTokenEx3.java
+++ src/stringtokenex3/StringTokenEx3.java
@@ -25,30 +25,50 @@ public class StringTokenEx3 {
         /*for (Object token1 : token) {
             System.out.println(token1);
         }*/
-        checkForInherite(token);
+        //checkForInherite(token);
         checkForObjectCreation(token);
         
-        
-
     }
     protected static void checkForInherite(ArrayList a) {
-        for (Object token1 : a) {
-            if (token1.equals("extends"))
+        int j = 0;
+        
+        for(int i = 0; i < a.size();i++ )
+        {
+            if(a.get(i).equals("extends"))
             {
                 System.out.println("Found Inherite");
             }
+            else
+            {
+                System.out.println("Not found");
+            }
         }
-            
+   
+
     }
     
     protected static void checkForObjectCreation(ArrayList a) {
-        for (Object token1 : a) {
-            if (token1.equals("new"))
+        //int j = 0;
+        String s0,s1,s2,s3;
+        for (int i = 3; i < a.size()-3; i++) {
+            
+            s0 = a.get(i).toString();
+            s1 = a.get(i-1).toString();
+            s2 = a.get(i+1).toString();
+            s3 = a.get(i-3).toString();
+            
+            /*System.out.println("s0: " + s0);
+            System.out.println("s1: " + s1);
+            System.out.println("s2: " + s2);
+            System.out.println("s3: " + s3);
+            */
+            if (s0.equals("new")&&(s1.equals("=")) && (s2.contains(s3)))
             {
+               
                 System.out.println("Found Object Creation.");
             }
-        }
+            //System.out.println(s0);
             
+        }
     }
-
 }
